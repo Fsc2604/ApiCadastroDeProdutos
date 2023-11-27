5a06a8c73a714c617696bd8cb29de92a461ead7a
Fabio Santana Carvalho <fabio.carvalho@intcom.com.br>
2023-11-27 12:26:30 -0300

    #Criação de DTO person + package Flluent Validation

---
+++ Api.CadastroDeProduto.Application/DTOs/PersonDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.CadastroDeProduto.Application.DTOs
+{
+    public class PersonDto
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Document { get; private set; }
+        public string Phone { get; private set; }
+    }
+}
---
+++ Api.CadastroDeProduto.Application/Validations/PersonDtoValidator.cs
@@ -0,0 +1,31 @@
+﻿using Api.CadastroDeProduto.Application.DTOs;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.CadastroDeProduto.Application.Validations
+{
+    public class PersonDtoValidator : AbstractValidator<PersonDto>
+    {
+        public PersonDtoValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Name deve ser informado");
+
+            RuleFor(x => x.Document)
+               .NotEmpty()
+               .NotNull()
+               .WithMessage("Document deve ser informado");
+
+            RuleFor(x => x.Phone)
+               .NotEmpty()
+               .NotNull()
+               .WithMessage("Phone deve ser informado");
+        }
+    }
+}
